64775e10cd1ca278daef48865a7eecfaa66f1701
aminul574 <a.soton7@gmail.com>
2023-12-08 04:02:11 +0600

    Blog repository testing added

--- repositories/auth.repository.js
+++ repositories/auth.repository.js
@@ -1,15 +1,14 @@
  const User = require("../models/user.model");
 
-const findEmailByUserEmail = async (email) => {
-  return await User.findOne({ where: { email: email } });
+const findEmailByUserEmail = (email) => {
+  return User.findOne({ where: { email: email } });
 };
 
 const getUserByUsername = (username) => {
   return User.findOne({ where: { username } });
 };
-const loginUser = async (username) => {
-  const res = await getUserByUsername(username);
-  return res;
+const loginUser = (username) => {
+  return getUserByUsername(username);
 };
 
 const createUser = async (username, email, hashPassword) => {
