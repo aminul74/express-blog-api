ed2f5607d5cda76f7c6c9aa6f2f062b6efc61eaa
aminul574 <a.soton7@gmail.com>
2023-12-10 12:43:27 +0600

    User repository testing added

--- middleware/user.validation.js
+++ middleware/user.validation.js
@@ -4,17 +4,17 @@ const validate = require("../middleware/validate.middleware");
 const signupSchema = yup.object({
   username: yup.string().required(),
   email: yup.string().email().required(),
-  password: yup.string().min(4).max(10).required(),
+  password: yup.string().min(4).max(12).required(),
 });
 
 const loginSchema = yup.object({
   username: yup.string().required(),
-  password: yup.string().min(4).max(10).required(),
+  password: yup.string().min(4).max(12).required(),
 });
 
 const passUpdateSchema = yup.object({
   old_password: yup.string().min(4).max(10).required(),
-  new_password: yup.string().min(4).max(10).required(),
+  new_password: yup.string().min(4).max(12).required(),
 });
 
 const validateSignup = validate(signupSchema);
--- repositories/user.repository.js
+++ repositories/user.repository.js
@@ -23,23 +23,7 @@ const getUserByUsername = (username) => {
   return User.findOne({ where: { username } });
 };
 
-// const findEmailByUserEmail = async (email) => {
-//   return await User.findOne({ where: { email: email } });
-// };
-
-// const createUser = async (username, email, hashPassword) => {
-//   return await User.create({ username, email, password: hashPassword });
-// };
-
-// const loginUser = async (username) => {
-//   const res = await getUserByUsername(username);
-//   return res;
-// };
-
 module.exports = {
-  // createUser,
-  // findEmailByUserEmail,
-  // loginUser,
   getUserByUsername,
   getUserById,
   deleteUserById,
